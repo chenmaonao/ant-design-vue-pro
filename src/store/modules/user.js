96e96c454ec3b5e08335b507c16571b95e951279
chenmaonao <447803922@qq.com>
2021-01-11 18:20:52 +0800

    feat: 自动登录

--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -1,7 +1,8 @@
 import storage from 'store'
-import { login, getInfo, logout } from '@/api/login'
+import { getInfo, logout } from '@/api/login'
 import { ACCESS_TOKEN } from '@/store/mutation-types'
 import { welcome } from '@/utils/util'
+import { dataList, sysData } from '@/const/user'
 
 const user = {
   state: {
@@ -10,7 +11,9 @@ const user = {
     welcome: '',
     avatar: '',
     roles: [],
-    info: {}
+    info: {},
+    sysData: [],
+    dataList: {}
   },
 
   mutations: {
@@ -29,6 +32,12 @@ const user = {
     },
     SET_INFO: (state, info) => {
       state.info = info
+    },
+    SET_SYSDATA: (state, data) => {
+      state.sysData = data
+    },
+    SET_DATALIST: (state, data) => {
+      state.dataList = data
     }
   },
 
@@ -36,64 +45,113 @@ const user = {
     // 登录
     Login ({ commit }, userInfo) {
       return new Promise((resolve, reject) => {
-        login(userInfo).then(response => {
-          const result = response.result
-          storage.set(ACCESS_TOKEN, result.token, 7 * 24 * 60 * 60 * 1000)
-          commit('SET_TOKEN', result.token)
+        console.log(userInfo)
+        if (userInfo.username === 'root' && userInfo.password === 'e10adc3949ba59abbe56e057f20f883e') {
+          storage.set(ACCESS_TOKEN, '---------', 7 * 24 * 60 * 60 * 1000)
+          commit('SET_TOKEN', '---------')
+          debugger
           resolve()
-        }).catch(error => {
-          reject(error)
-        })
+        } else {
+          reject(new Error())
+        }
+        // login(userInfo).then(response => {
+        //   const result = response.result
+        //   storage.set(ACCESS_TOKEN, result.token, 7 * 24 * 60 * 60 * 1000)
+        //   commit('SET_TOKEN', result.token)
+        //   resolve()
+        // }).catch(error => {
+        //   reject(error)
+        // })
+      })
+    },
+
+    // 保存信息
+    SetUserAndSys ({ commit }, info) {
+      console.log(info)
+      return new Promise((resolve, reject) => {
+        commit('SET_SYSDATA', info.sysData)
+        commit('SET_DATALIST', info.dataList)
+        storage.set('SET_SYSDATA', info.sysData, 365 * 24 * 60 * 60 * 1000)
+        storage.set('SET_DATALIST', info.dataList, 365 * 24 * 60 * 60 * 1000)
+        resolve()
+      })
+    },
+
+    GetUserAndSys ({ commit }, info) {
+      return new Promise((resolve, reject) => {
+        const sysDataStorage = storage.get('SET_SYSDATA')
+        const dataListStorage = storage.get('SET_DATALIST')
+        console.log('sysDataStorage', sysData)
+        console.log('dataListStorage', dataList)
+        if (sysDataStorage) {
+          commit('SET_SYSDATA', sysDataStorage)
+        } else {
+          commit('SET_SYSDATA', sysData)
+        }
+        if (dataListStorage) {
+          commit('SET_DATALIST', dataListStorage)
+        } else {
+          commit('SET_DATALIST', dataList)
+        }
+        resolve()
       })
     },
 
     // 获取用户信息
-    GetInfo ({ commit }) {
+    GetInfo ({ commit, dispatch }) {
       return new Promise((resolve, reject) => {
-        getInfo().then(response => {
-          const result = response.result
+        dispatch('GetUserAndSys')
+        getInfo()
+          .then(response => {
+            const result = response.result
 
-          if (result.role && result.role.permissions.length > 0) {
-            const role = result.role
-            role.permissions = result.role.permissions
-            role.permissions.map(per => {
-              if (per.actionEntitySet != null && per.actionEntitySet.length > 0) {
-                const action = per.actionEntitySet.map(action => { return action.action })
-                per.actionList = action
-              }
-            })
-            role.permissionList = role.permissions.map(permission => { return permission.permissionId })
-            commit('SET_ROLES', result.role)
-            commit('SET_INFO', result)
-          } else {
-            reject(new Error('getInfo: roles must be a non-null array !'))
-          }
+            if (result.role && result.role.permissions.length > 0) {
+              const role = result.role
+              role.permissions = result.role.permissions
+              role.permissions.map(per => {
+                if (per.actionEntitySet != null && per.actionEntitySet.length > 0) {
+                  const action = per.actionEntitySet.map(action => {
+                    return action.action
+                  })
+                  per.actionList = action
+                }
+              })
+              role.permissionList = role.permissions.map(permission => {
+                return permission.permissionId
+              })
+              commit('SET_ROLES', result.role)
+              commit('SET_INFO', result)
+            } else {
+              reject(new Error('getInfo: roles must be a non-null array !'))
+            }
 
-          commit('SET_NAME', { name: result.name, welcome: welcome() })
-          commit('SET_AVATAR', result.avatar)
+            commit('SET_NAME', { name: result.name, welcome: welcome() })
+            commit('SET_AVATAR', result.avatar)
 
-          resolve(response)
-        }).catch(error => {
-          reject(error)
-        })
+            resolve(response)
+          })
+          .catch(error => {
+            reject(error)
+          })
       })
     },
 
     // 登出
     Logout ({ commit, state }) {
-      return new Promise((resolve) => {
-        logout(state.token).then(() => {
-          commit('SET_TOKEN', '')
-          commit('SET_ROLES', [])
-          storage.remove(ACCESS_TOKEN)
-          resolve()
-        }).catch(() => {
-          resolve()
-        }).finally(() => {
-        })
+      return new Promise(resolve => {
+        logout(state.token)
+          .then(() => {
+            commit('SET_TOKEN', '')
+            commit('SET_ROLES', [])
+            storage.remove(ACCESS_TOKEN)
+            resolve()
+          })
+          .catch(() => {
+            resolve()
+          })
+          .finally(() => {})
       })
     }
-
   }
 }
 
